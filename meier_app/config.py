d9bc3aa306ff2238fbd28d0b1776657653aebbe2
AhnSeongHyun <sh84.ahn@gmail.com>
2018-03-08 22:47:16 +0900

    add prev, next in post_list and modify migration_heaven for command line user input

--- meier_app/config.py
+++ meier_app/config.py
@@ -1,4 +1,4 @@
 meier_config = {
-    'SQLALCHEMY_DATABASE_URI':'mysql+pymysql://<ID>:<PASSWORD>@<HOST>:<PORT>/meier'
+    'SQLALCHEMY_DATABASE_URI':'mysql+pymysql://ash84:ash84@ash84.net:3306/meier'
 
 }
--- meier_app/resources/blog/post_list_view.py
+++ meier_app/resources/blog/post_list_view.py
@@ -1,23 +1,33 @@
 # -*- coding:utf-8 -*-
 from flask import Blueprint, render_template
+from flask import request
 
-from meier_app.models.post import Post
+from meier_app.commons.logger import logger
+from meier_app.models.post import Post, PostVisibility, PostStatus
 from meier_app.models.setting import Settings
+from sqlalchemy import desc
 
 post_list_view = Blueprint('post_list_view', __name__, url_prefix='/')
 
 
 @post_list_view.route('', methods=['GET'])
 def get_post_list_view():
-    from sqlalchemy import desc
-
+    page = request.args.get('page', 1)
+    
     settings = Settings.query.first()
-    # todo : POST-TAG RELATION SHIP
-    post_list = [post.for_detail for post in Post.query.order_by(desc(Post.in_date)).all()] # todo : pagination using settings page_per_post
-    # todo : settings 를 처음에 로딩시 app.config 에 넣어 두던지 해야할듯.
+    post_paging_result = Post.query.filter(Post.status == PostStatus.PUBLISH.value)\
+        .filter(Post.visibility == PostVisibility.PUBLIC.value)\
+        .order_by(desc(Post.in_date)).paginate(page, settings.post_per_page, error_out=False)
+
+    post_list = [post.for_detail for post in post_paging_result.items]
 
     return render_template("/themes/" + settings.theme + "/post_list.html",
                            blog_title=settings.blog_title,
                            blog_desc=settings.blog_desc,
-                           post_list=post_list
+                           post_list=post_list,
+                           has_next=post_paging_result.has_next,
+                           next="/page/{}".format(page+1),
+                           has_prev=post_paging_result.has_prev,
+                           prev=next="/page/{}".format(page-1)
                            )
+                           
