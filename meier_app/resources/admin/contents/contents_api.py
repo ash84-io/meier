90cb95c8a6556aa4376c440fe7dec35cd58bb4b8
anseonghyun <sh84.ahn@gmail.com>
2018-03-20 05:36:43 +0900

    add files

--- meier_app/resources/admin/contents/contents_api.py
+++ meier_app/resources/admin/contents/contents_api.py
@@ -18,7 +18,7 @@ def get_contents_posts_api():
     page = int(request.args.get('page', 1))
     per_page = int(request.args.get('perPage', 10))
 
-    post_paging_result = Post.query.order_by(desc(Post.in_date)).paginate(page, per_page, error_out=False)
+    post_paging_result = Post.query.filter(Post.status == PostStatus.PUBLISH.value).order_by(desc(Post.in_date)).paginate(page, per_page, error_out=False)
     post_list = [post.for_admin for i, post in enumerate(post_paging_result.items)]
 
     logger.debug(len(post_list))
@@ -28,4 +28,18 @@ def get_contents_posts_api():
     return ResponseData(code=HttpStatusCode.SUCCESS, data={'items':post_list, 'page':page, 'total': post_paging_result.total}).json
 
 
+@admin_contents_api.route('/draft', methods=['GET'])
+@login_required
+def get_contents_draft_api():
+    logger.debug(request.args)
+    page = int(request.args.get('page', 1))
+    per_page = int(request.args.get('perPage', 10))
 
+    post_paging_result = Post.query.filter(Post.status == PostStatus.DRAFT.value).order_by(desc(Post.in_date)).paginate(page, per_page, error_out=False)
+    post_list = [post.for_admin for i, post in enumerate(post_paging_result.items)]
+
+    logger.debug(len(post_list))
+    for post in post_list:
+        logger.debug(post['title'])
+
+    return ResponseData(code=HttpStatusCode.SUCCESS, data={'items':post_list, 'page':page, 'total': post_paging_result.total}).json
