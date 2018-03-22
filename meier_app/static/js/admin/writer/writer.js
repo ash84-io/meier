027b27c2c484233accce98a87d36d4494db49e70
AhnSeongHyun <sh84.ahn@gmail.com>
2018-03-22 20:47:04 +0900

    code for writer

--- meier_app/static/js/admin/writer/writer.js
+++ meier_app/static/js/admin/writer/writer.js
@@ -1,10 +1,25 @@
 
-var editor = new tui.Editor({
-    el: document.querySelector('#editor'),
-    initialEditType: 'markdown',
-    previewStyle: 'vertical',
-    height: '600px',
-    codeBlockLanguages: ['python', 'javascript'],
-    useCommandShortcut: true,
-    exts: ['scrollSync', 'colorSyntax', 'uml', 'chart', 'mark', 'table', 'taskCounter']
+var vm;
+vm = new Vue({
+    el: '#vue-section',
+    mounted: function () {
+        var editor = new tui.Editor({
+            el: document.querySelector('#editor'),
+            initialEditType: 'markdown',
+            previewStyle: 'vertical',
+            height: '600px',
+            codeBlockLanguages: ['python', 'javascript'],
+            useCommandShortcut: true,
+            exts: ['scrollSync', 'colorSyntax', 'uml', 'chart', 'mark', 'table', 'taskCounter']
+        });
+    },
+    methods: {
+        save: function (event) {
+            console.log("save");
+        },
+        delete: function(event){
+            console.log("delete");
+        }
+    }
+
 });
