6fac8aa94065fc4e7225f1898fcadde56812da62
anseonghyun <sh84.ahn@gmail.com>
2018-05-28 07:42:44 +0900

    fix logger for testing

--- meier_app/commons/logger.py
+++ meier_app/commons/logger.py
@@ -2,15 +2,15 @@
 import logging
 from logging import Formatter
 from logging import FileHandler, StreamHandler
-
+from flask import current_app
 logger = logging.getLogger('meier_app.logger')
 logging.basicConfig()
 stream_handler = StreamHandler()
-log_file_handler = FileHandler(filename="./log/meier.log", mode='a', encoding="utf-8")
 log_formatter = Formatter("[%(process)d:%(processName)s:%(thread)d:%(threadName)s] %(asctime)s : %(message)s [in %(filename)s:%(lineno)d]")
-log_file_handler.setFormatter(log_formatter)
+if current_app.testing:
+    log_file_handler = FileHandler(filename="./log/meier.log", mode='a', encoding="utf-8")
+    logger.addHandler(log_file_handler)
+    log_file_handler.setFormatter(log_formatter)
 stream_handler.setFormatter(log_formatter)
-
 logger.setLevel(logging.DEBUG)
-logger.addHandler(log_file_handler)
 logger.addHandler(stream_handler)
