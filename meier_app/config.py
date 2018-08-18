67ecd7f90d5e6d4c76091287716723900028d03b
anseonghyun <sh84.ahn@gmail.com>
2018-08-18 15:37:55 +0900

    markdown prefix "language-" for prism.js

--- meier_app/config.py
+++ meier_app/config.py
@@ -4,6 +4,7 @@ db_host = os.getenv('db_host', None)
 db_user = os.getenv('db_user', None)
 db_password = os.getenv('db_password', None)
 
+
 class Config(object):
     DEBUG = False
     TESTING = False
@@ -14,14 +15,14 @@ class Config(object):
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@{}/meier'.format(db_user, db_password, db_host)
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ash84:ash8467501!@ash84.net/meier'
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@{}/meier'.format(db_user, db_password, db_host)
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ash84:ash8467501!@ash84.net/meier'
 
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@{}/meier_test'.format(db_user, db_password, db_host)
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ash84:ash8467501!@ash84.net/meier_test'
