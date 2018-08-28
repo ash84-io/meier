55f3e6857de90dcee230c0428d6b14bba16ce3a9
AhnSeognHyun <sh84.ahn@gmail.com>
2018-08-28 16:54:44 +0900

    add model testcases

--- meier_app/tests/test_models/test_post_models.py
+++ meier_app/tests/test_models/test_post_models.py
@@ -1,20 +1,89 @@
-import pytest
 from mixer.backend.flask import mixer
+from meier_app.tests.conftest import session
 
-from meier_app.extensions import db
-from meier_app.models.post import Post
-from meier_app.tests.base import BaseModelTestCase
-from meier_app.tests.base import app as mock_app
+from meier_app.models.post import Post, PostStatus, PostVisibility
 
 
-class TestPostModels(BaseModelTestCase):
+def test_insert(session):
+    p1 = mixer.blend(Post)
+    p2 = mixer.blend(Post,
+                     post_name="test",
+                     title="test",
+                     content="test",
+                     html="test",
+                     is_page=False,
+                     visibility=PostVisibility.PUBLIC,
+                     status=PostStatus.DRAFT,
+                     featured_image="test"
+                     )
 
-    def test_insert(self):
-        pass
+    assert p2.post_name == 'test'
+    assert p2.title == 'test'
+    assert p2.content == 'test'
+    assert p2.html == 'test'
+    assert p2.visibility == PostVisibility.PUBLIC
+    assert p2.status == PostStatus.DRAFT
+    assert p2.featured_image == "test"
+    assert Post.query.count() == 2
 
-    def test_update(self):
-        pass
+    for_detail_keys = {'title': str,
+                       'content': str,
+                       'html': str,
+                       'created_at': str,
+                       'modified_at': str,
+                       'link': str,
+                       'featured_image': str,
+                       'is_page': bool}
 
-    def test_delete(self):
-        pass
+    for k, v in p2.for_detail.items():
+        assert isinstance(v, for_detail_keys[k])
+
+    for_admin_keys = {
+        'id': int,
+        'raw_content': str,
+        'post_name': str,
+        'title': str,
+        'content': str,
+        'html': str,
+        'created_at': str,
+        'modified_at': str,
+        'link': str,
+        'featured_image': str,
+        'is_page': bool
+    }
+
+    for k, v in p2.for_admin.items():
+        assert isinstance(v, for_admin_keys[k])
+
+
+def test_update(session):
+    mixer.blend(Post)
+
+    p = Post.query.scalar()
+    p.post_name = "blog"
+    p.title = "blog"
+    p.post_name = 'blog'
+    p.title = 'blog'
+    p.content = 'blog'
+    p.html = 'blog'
+    p.visibility = PostVisibility.PRIVATE
+    p.status = PostStatus.PUBLISH
+    p.featured_image = "blog"
+
+    session.commit()
+    assert p.post_name == 'blog'
+    assert p.title == 'blog'
+    assert p.content == 'blog'
+    assert p.html == 'blog'
+    assert p.visibility == PostVisibility.PRIVATE
+    assert p.status == PostStatus.PUBLISH
+    assert p.featured_image == "blog"
+ 
+
+def test_delete(session):
+    mixer.blend(Post)
+    p = Post.query.scalar()
+    session.delete(p)
+    session.commit()
+    assert Post.query.count() == 0
 
--- meier_app/tests/test_models/test_post_tag_models.py
+++ meier_app/tests/test_models/test_post_tag_models.py
@@ -1,20 +1,42 @@
-import pytest
 from mixer.backend.flask import mixer
-
-from meier_app.extensions import db
+from meier_app.tests.conftest import session
 from meier_app.models.post_tag import PostTag
-from meier_app.tests.base import BaseModelTestCase
-from meier_app.tests.base import app as mock_app
 
 
-class TestPostTagModels(BaseModelTestCase):
+def test_insert(session):
+    mixer.blend(PostTag)
+    mixer.blend(PostTag,
+                post_id=1,
+                tag_id=2
+                )
+    session.commit()
+    assert PostTag.query.count() == 2
 
-    def test_insert(self):
-        pass
 
-    def test_update(self):
-        pass
+def test_update(session):
+    p1 = mixer.blend(PostTag,
+                     post_id=1,
+                     tag_id=2
+                     )
+    assert p1.post_id == 1
+    assert p1.tag_id == 2
 
-    def test_delete(self):
-        pass
+    p1.post_id = 2
+    p1.tag_id = 3
+    session.commit()
+    p = PostTag.query.scalar()
+    assert p.post_id == 2
+    assert p.tag_id == 3
+
+
+def test_delete(session):
+    p1 = mixer.blend(PostTag,
+                     post_id=1,
+                     tag_id=2
+                     )
+    session.delete(p1)
+    session.commit()
+    assert PostTag.query.count() == 0
+
+
 
