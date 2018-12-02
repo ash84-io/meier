69f12c9289900f473f5ff2cd16a0d264d705156a
AhnSeongHyun <sh84.ahn@gmail.com>
2018-12-02 21:16:49 +0900

    Add aes256 and jwt settings

--- meier_app/config.py
+++ meier_app/config.py
@@ -1,8 +1,9 @@
 import os
 
-db_host = os.getenv('db_host', None)
-db_user = os.getenv('db_user', None)
-db_password = os.getenv('db_password', None)
+db_host = os.getenv('db_host', "ash84.net")
+db_user = os.getenv('db_user', "ash84")
+db_password = os.getenv('db_password', "ash8467501!")
+sentry_dsn = os.getenv('sentry_dsn', '')
 
 
 class Config(object):
@@ -12,6 +13,11 @@ class Config(object):
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_POOL_RECYCLE = 20
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    JWT_SECRET_KEY = 'meier_jwt'
+    JWT_TOKEN_LOCATION = 'cookies'
+    JWT_COOKIE_SECURE = True
+    AES256_KEY = 'MEIER_ENC_KEY_20181202!@#$%^&*()'
+    AES256_IV = 'MEIER_IV20181202'
 
 
 class ProductionConfig(Config):
