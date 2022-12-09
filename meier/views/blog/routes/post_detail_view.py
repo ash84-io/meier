e7ae6516f91f68b1388a77569ed0b155869826db
ash84 <sh84.ahn@gmail.com>
2022-12-09 13:44:48 +0900

    fix: opengraph (#207)

--- meier/views/blog/routes/post_detail_view.py
+++ meier/views/blog/routes/post_detail_view.py
@@ -5,7 +5,7 @@ from meier.models.post_tag import PostTag
 from meier.models.settings import Settings
 from meier.models.tag import Tag
 from meier.models.user import User
-from meier.views.blog.services.open_graph import OpenGraphMetaTagGenerator
+from meier.views.blog.services.opengraph import OpenGraphMetaTagGenerator
 
 post_detail_view = Blueprint("post_detail_view", __name__, url_prefix="")
 
